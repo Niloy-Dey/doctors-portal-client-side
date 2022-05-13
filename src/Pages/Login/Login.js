b5d399b81ba348eebf4e0a68cae90821fff2e4e7
Niloy-Dey <niloydeyce@gmail.com>
2022-05-13 21:25:22 +0600

    sign in google done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,49 @@
 import React from 'react';
-
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 const Login = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user);
+    }
+
     return (
-        <div>
-            <h1>hello login</h1>
+        <div className='pt-20 '>
+            <h1 class="text-5xl font-bold text-center text-secondary">Login now!</h1>
+            <div class="hero ">
+                <div class="hero-content w-full flex-col lg:flex-row-reverse">
+                    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div class="card-body">
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" class="input input-bordered" />
+                            </div>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" class="input input-bordered" />
+                                <label class="label">
+                                    <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div class="form-control mt-6">
+                                <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary ">Login</button>
+                            </div>
+
+
+                                <p>New to Doctors Portal ? <a href=" " className='text-secondary'>Create a New Account</a>  </p>
+
+
+                            <div className='divider'>OR</div>
+                            <button onClick={() => signInWithGoogle()} className='btn btn-outline '>Continue With Google</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey:process.env.REACT_APP_API_KEY,
   authDomain:process.env.REACT_APP_AUTH_DOMAIN ,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+
+ /*  apiKey: "AIzaSyDCdnE6aon9yttpu9JBnOzsEgn-B67pvoI",
+  authDomain: "doctors-portal-535fa.firebaseapp.com",
+  projectId: "doctors-portal-535fa",
+  storageBucket: "doctors-portal-535fa.appspot.com",
+  messagingSenderId: "4976188358",
+  appId: "1:4976188358:web:4a943d573891331937f0c7", */
 };
 
 // Initialize Firebase
